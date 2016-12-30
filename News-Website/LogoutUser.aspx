90e7f21e30230d68d8aff59923412f7ebe12a83b
Adrian Ispas <adi.ispas95@gmail.com>
2016-12-30 15:19:01 +0200

    Finished Logout page.

---
+++ News-Website/LogoutUser.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/OtherPage.master" AutoEventWireup="true" CodeFile="LogoutUser.aspx.cs" Inherits="LogoutUser" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <p>
+        Sunteți conectat cu userul
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        .</p>
+    <p>
+        Sigur doriți să vă deconectați?</p>
+    <p>
+        <asp:Button ID="Button1" runat="server" Text="Da" OnClick="Button1_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Text="Nu" OnClick="Button2_Click" />
+    </p>
+</asp:Content>
+
---
+++ News-Website/LogoutUser.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class LogoutUser : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.Title = "Logout Page - News Website";
+
+        if (CheckLogin() == false)
+            Response.Redirect("LoginUser.aspx", true);
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Index.aspx");
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Session["user"] = null;
+        Session["pass"] = null;
+        Response.Redirect("Index.aspx");
+    }
+
+    private bool CheckLogin()
+    {
+        //daca nu avem user trebuie sa ne logam
+        // poate a expirat sesiunea sau poate cineva vrea sa intre prin URL
+        if (Session["user"] == null || string.IsNullOrEmpty(Session["user"].ToString()))
+            return false;
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        string txt = "SELECT password FROM Utilizator WHERE (name=@Name)";
+        // deschiderea conexiunii. Poate arunca Exceptie daca nu reuseste
+        conn.Open();
+        //crearea comenzi SQL
+        SqlCommand cmd = new SqlCommand(txt, conn);
+        //adaugarea parametrilor si definirea tipului lor
+        cmd.Parameters.Add(new SqlParameter("@Name", TypeCode.String));
+        cmd.Parameters["@Name"].Value = Session["user"].ToString();
+        //scalar returneaza o singura valoare
+        SqlDataReader reader = cmd.ExecuteReader();
+        if (reader.Read())
+        {
+            string hashpass = (string)reader[0];
+            hashpass = hashpass.Trim();
+            if (Session["pass"].ToString() == hashpass)
+            {
+                conn.Close();
+                return true;
+            }
+        }
+        conn.Close();
+        return false;
+    }
+}
