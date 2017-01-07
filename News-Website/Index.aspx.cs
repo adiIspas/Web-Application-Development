b045a8b20ef5246bc5b3e637c901dcc53977f1ae
Adrian Ispas <adi.ispas95@gmail.com>
2017-01-07 19:39:56 +0200

    Update.

--- News-Website/Index.aspx.cs
+++ News-Website/Index.aspx.cs
@@ -9,7 +9,7 @@ public partial class Index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+
     }
 
     protected void last_news_SelectedIndexChanged(object sender, EventArgs e)
